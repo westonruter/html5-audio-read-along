e4e8aec05511ad4aad8c54085f506a3696073297
Weston Ruter <westonruter@gmail.com>
2012-04-28 00:11:03 +1000

    Rewrite of getCurrentWord; attempt to add range for playbackRate

--- read-along.js
+++ read-along.js
@@ -1,21 +1,14 @@
 var ReadAlong = {
     text_element: null,
     audio_element: null,
+    rate_control_element: null,
     
     words: [],
     
-    current_start_word: null,
-    current_end_word: null,
-    current_selected_range: null,
-    
-    /**
-     * The timeupdate Media event does not fire repeatedly enough to be
-     * able to rely on for updating the selected word (it hovers around
-     * 250ms resolution), so we add a setTimeout with the exact duration
-     * of the word.
-     */
-    select_next_timeout_id: null,
-    
+    //current_begin_word: null,
+    //current_end_word: null,
+    //current_selected_range: null,
+        
     init: function (args) {
         var name;
         for (name in args) {
@@ -25,7 +18,8 @@ var ReadAlong = {
         this.generateWordList();
         this.addEventListeners();
         
-        // Once set up, unhide audio player
+        // Once set up, enable the UI
+        this.rate_control_element.disabled = false;
         this.audio_element.hidden = false;
     },
     
@@ -55,10 +49,10 @@ var ReadAlong = {
     getCurrentWord: function () {
         var i;
         var len;
-        var is_next_word;
+        var is_current_word;
         var word = null;
         for (i = 0, len = this.words.length; i < len; i += 1) {
-            is_next_word = (
+            is_current_word = (
                 (
                     this.audio_element.currentTime >= this.words[i].begin
                     &&
@@ -67,50 +61,65 @@ var ReadAlong = {
                 ||
                 (this.audio_element.currentTime < this.words[i].begin)
             );
-            if (is_next_word) {
+            if (is_current_word) {
                 word = this.words[i];
                 break;
             }
         }
+        
+        if (!word) {
+            throw Error('Unable to find current word and we should always be able to.');
+        }
         return word;
     },
     
-    /**
-     * Select the next word that is going to be read or the word that is being read right now
-     * @param {Boolean} is_automatic_advance  Whether or not the subsequent word should automatically be selected
-     */
-    selectNextWord: function(is_automatic_advance) {
-        var next_word;
-        var that = this;
-        var select_word = function (is_automatic_advance) {
-            that.removeWordSelection();
-            next_word.element.classList.add('speaking');
-            if (!is_automatic_advance) {
-                var delay = Math.round( (next_word.end - that.audio_element.currentTime) * 1000 );
-                clearTimeout(that.select_next_timeout_id);
-                that.select_next_timeout_id = setTimeout(function () {
-                    that.removeWordSelection();
-                    if (!that.audio_element.paused) {
-                        that.selectNextWord();
-                    }
-                }, delay);
-            }
-        };
     
-        clearTimeout(this.select_next_timeout_id);
-        next_word = this.getCurrentWord();
-        if (next_word) {
-            //Select now
-            if(is_automatic_advance || that.audio_element.currentTime >= next_word.begin){
-                select_word(is_automatic_advance);
-            }
-            //Select later
-            else {
-                var delay = Math.round( (next_word.begin - this.audio_element.currentTime) * 1000 );
-                clearTimeout(this.select_next_timeout_id);
-                this.select_next_timeout_id = setTimeout(select_word, delay);
+    /**
+     * Select the current word and set timeout to select the next one if playing
+     */
+    selectCurrentWord: function() {
+        var that = this;
+        var current_word = this.getCurrentWord();
+        var is_playing = !this.audio_element.paused;
+        
+        this.removeWordSelection();
+        current_word.element.classList.add('speaking');
+
+        /**
+         * The timeupdate Media event does not fire repeatedly enough to be
+         * able to rely on for updating the selected word (it hovers around
+         * 250ms resolution), so we add a setTimeout with the exact duration
+         * of the word.
+         * @todo We will have to multiple the the seconds by the speech rate!
+         */
+        if (is_playing) {
+            // Remove word selection when the word ceases to be spoken
+            var seconds_until_this_word_ends = current_word.end - this.audio_element.currentTime; // Note: 'word' not 'world'! ;-)
+            seconds_until_this_word_ends *= this.audio_element.playbackRate;
+            setTimeout(
+                function () {
+                    if (!that.audio_element.paused) { // we always want to have a word selected while paused
+                        current_word.element.classList.remove('speaking');
+                    }
+                },
+                (seconds_until_this_word_ends * 1000)
+            );
+
+            // Automatically trigger selectCurrentWord when the next word begins
+            var next_word = this.words[current_word.index + 1];
+            if (next_word) {
+                var seconds_until_next_word_begins = next_word.begin - this.audio_element.currentTime;
+                seconds_until_next_word_begins *= this.audio_element.playbackRate;
+                setTimeout(
+                    function () {
+                        that.selectCurrentWord();
+                    },
+                    (seconds_until_next_word_begins * 1000)
+                );
+                
             }
         }
+        
     },
     
     removeWordSelection: function() {
@@ -125,52 +134,65 @@ var ReadAlong = {
     addEventListeners: function () {
         var that = this;
         
+        var on_rate_control_change = function () {
+            var output = this.parentNode.getElementsByTagName('output')[0];
+            output.textContent = String(Math.round(this.valueAsNumber*10)/10);
+            that.audio_element.playbackRate = this.valueAsNumber;
+        };
+        that.rate_control_element.addEventListener('change', on_rate_control_change);
+        on_rate_control_change.call(that.rate_control_element);
+        
         /**
-         * Select next word (at that.audio_element.currentTime) when playing starts
+         * Select next word (at that.audio_element.currentTime) when playing begins
          */
         that.audio_element.addEventListener('play', function () {
             
-            var selection = window.getSelection();
-            if (selection.rangeCount !== 0) {
-                var range = selection.getRangeAt(0);
-                var start_word_node = that.selectParentNodeUntilWord(range.startContainer);
-                var end_word_node = that.selectParentNodeUntilWord(range.endContainer);
-                
-                var is_passage_selected = (
-                    !!range
-                    &&
-                    !range.collapsed
-                    &&
-                    that.text_element.contains(range.commonAncestorContainer)
-                );
-                
-                if (is_passage_selected) {
-                    that.current_selected_range = range;
-                    //selection.removeAllRanges();
-                }
-                //console.info(is_passage_selected, range);
-        
-            }
+            //var selection = window.getSelection();
+            //if (selection.rangeCount !== 0) {
+            //    var range = selection.getRangeAt(0);
+            //    var begin_word_el = that._selectParentNodeUntilWord(range.startContainer);
+            //    var end_word_el = that._selectParentNodeUntilWord(range.endContainer);
+            //    
+            //    var is_passage_selected = (
+            //        !!range
+            //        &&
+            //        !range.collapsed
+            //        &&
+            //        that.text_element.contains(range.commonAncestorContainer)
+            //        &&
+            //        begin_word_el
+            //        &&
+            //        end_word_el
+            //    );
+            //    
+            //    if (is_passage_selected) {
+            //        //that.current_begin_word = begin_word_node;
+            //        //that.current_end_word = end_word_node;
+            //        that.audio_element.currentTime = that.words[begin_word_el.dataset.index].begin;
+            //        that.current_selected_range = range;
+            //        selection.removeAllRanges();
+            //    }
+            //
+            //}
             
-            that.selectNextWord();
+            that.selectCurrentWord();
             that.text_element.classList.add('speaking');
-            
         });
         
         /**
          * Abandon seeking the next word because we're paused
          */
         that.audio_element.addEventListener('pause', function () {
-            clearTimeout(that.select_next_timeout_id);
+            that.selectCurrentWord(); // We always want a word to be selected
             that.text_element.classList.remove('speaking');
             
             // if there was a selection, restore it.
-            if (that.current_selected_range !== null) {
-                var selection = window.getSelection();
-                selection.removeAllRanges();
-                selection.addRange(that.current_selected_range);
-                that.current_selected_range = null;
-            }
+            //if (that.current_selected_range !== null) {
+            //    var selection = window.getSelection();
+            //    selection.removeAllRanges();
+            //    selection.addRange(that.current_selected_range);
+            //    that.current_selected_range = null;
+            //}
         });
         
         /**
@@ -178,19 +200,18 @@ var ReadAlong = {
          */
         that.text_element.addEventListener('click', function(e) {
             
-            if (!e.target.hasAttribute('data-begin')) {
+            if (!e.target.dataset.begin) {
                 return;
             }
             e.preventDefault();
             
-            var selection = window.getSelection();
-            
-            that.current_start_word = e.target;
-            that.current_end_word = e.target;
+            //var selection = window.getSelection();
+            //that.current_begin_word = e.target;
+            //that.current_end_word = e.target;
         
             var i = e.target.dataset.index;
-            that.audio_element.currentTime = that.words[i].begin + 0.001; //Note: times apparently cannot be exactly set and sometimes select too early
-            that.selectNextWord();
+            that.audio_element.currentTime = that.words[i].begin + 0.01; //Note: times apparently cannot be exactly set and sometimes select too early
+            that.selectCurrentWord();
         });
         
         /**
@@ -207,11 +228,7 @@ var ReadAlong = {
          * Select a word when seeking
          */
         that.audio_element.addEventListener('seeked', function (e) {
-            that.selectNextWord(e.target.paused /* for is_automatic_advance, probably always true */);
-        });
-        
-        that.audio_element.addEventListener('timeupdate', function (e){
-            
+            that.selectCurrentWord();
         });
     },
     
@@ -219,7 +236,7 @@ var ReadAlong = {
         if ( !node || !this.text_element.contains(node) ){
             return null;
         }
-        var is_word_node = (node.nodeType === 1 && node.hasAttribute('data-begin'));
+        var is_word_node = (node.nodeType === 1 && node.dataset.index);
         if (is_word_node) {
             return node;
         }
