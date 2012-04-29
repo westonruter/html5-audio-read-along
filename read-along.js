1efa60098ce4a2b84e9518c425d2808c9e663780
Weston Ruter <westonruter@gmail.com>
2012-04-29 23:40:31 +1000

    Keyboard accessability, better timeouts for highlights (with rate), better error messaging

--- read-along.js
+++ read-along.js
@@ -1,26 +1,26 @@
+// @todo Allow selection of a portion of text to have it read.
+// @todo Make keyboard accessible
 var ReadAlong = {
     text_element: null,
     audio_element: null,
-    
+    rate_range_element: null,
+
     words: [],
-    
+
     //current_begin_word: null,
     //current_end_word: null,
     //current_selected_range: null,
-        
+
     init: function (args) {
         var name;
         for (name in args) {
             this[name] = args[name];
         }
-        
         this.generateWordList();
         this.addEventListeners();
-        
-        // Once set up, enable the UI
-        this.audio_element.hidden = false;
+        this.selectCurrentWord();
     },
-    
+
     /**
      * Build an index of all of the words that can be read along with their begin,
      * and end times, and the DOM element representing the word.
@@ -33,13 +33,14 @@ var ReadAlong = {
                 'dur': parseFloat(word_el.dataset.dur),
                 'element': word_el
             };
+            word_el.setAttribute('tabindex', '0');
             word.index = index;
             word.end = word.begin + word.dur;
             word_el.dataset.index = word.index;
             return word;
         });
     },
-        
+
     /**
      * From the audio's currentTime, find the word that is currently being played
      * @todo this would better be implemented as a binary search
@@ -64,14 +65,16 @@ var ReadAlong = {
                 break;
             }
         }
-        
+
         if (!word) {
             throw Error('Unable to find current word and we should always be able to.');
         }
         return word;
     },
-    
-    
+
+    _current_end_select_timeout_id: null,
+    _current_next_select_timeout_id: null,
+
     /**
      * Select the current word and set timeout to select the next one if playing
      */
@@ -79,10 +82,11 @@ var ReadAlong = {
         var that = this;
         var current_word = this.getCurrentWord();
         var is_playing = !this.audio_element.paused;
-        
+
         if (!current_word.element.classList.contains('speaking')) {
             this.removeWordSelection();
             current_word.element.classList.add('speaking');
+            current_word.element.focus();
         }
 
         /**
@@ -95,33 +99,40 @@ var ReadAlong = {
         if (is_playing) {
             // Remove word selection when the word ceases to be spoken
             var seconds_until_this_word_ends = current_word.end - this.audio_element.currentTime; // Note: 'word' not 'world'! ;-)
-            seconds_until_this_word_ends *= this.audio_element.playbackRate;
-            setTimeout(
+            if (typeof this.audio_element === 'number' && !isNaN(this.audio_element)) {
+                seconds_until_this_word_ends *= 1.0/this.audio_element.playbackRate;
+            }
+            clearTimeout(this._current_end_select_timeout_id);
+            this._current_end_select_timeout_id = setTimeout(
                 function () {
                     if (!that.audio_element.paused) { // we always want to have a word selected while paused
                         current_word.element.classList.remove('speaking');
                     }
                 },
-                (seconds_until_this_word_ends * 1000)
+                Math.max(seconds_until_this_word_ends * 1000, 0)
             );
 
             // Automatically trigger selectCurrentWord when the next word begins
             var next_word = this.words[current_word.index + 1];
             if (next_word) {
                 var seconds_until_next_word_begins = next_word.begin - this.audio_element.currentTime;
-                seconds_until_next_word_begins *= this.audio_element.playbackRate;
-                setTimeout(
+
+                var orig_seconds_until_next_word_begins = seconds_until_next_word_begins; // temp
+                if (typeof this.audio_element === 'number' && !isNaN(this.audio_element)) {
+                    seconds_until_next_word_begins *= 1.0/this.audio_element.playbackRate;
+                }
+                clearTimeout(this._current_next_select_timeout_id);
+                this._current_next_select_timeout_id = setTimeout(
                     function () {
                         that.selectCurrentWord();
                     },
-                    (seconds_until_next_word_begins * 1000)
+                    Math.max(seconds_until_next_word_begins * 1000, 0)
                 );
-                
             }
         }
-        
+
     },
-    
+
     removeWordSelection: function() {
         // There should only be one element with .speaking, but selecting all for good measure
         var spoken_word_els = this.text_element.querySelectorAll('span[data-begin].speaking');
@@ -129,23 +140,23 @@ var ReadAlong = {
             spoken_word_el.classList.remove('speaking');
         });
     },
-    
-    
+
+
     addEventListeners: function () {
         var that = this;
-        
+
         /**
          * Select next word (at that.audio_element.currentTime) when playing begins
          */
         that.audio_element.addEventListener('play', function (e) {
             console.info(e.type); // debug
-            
+
             //var selection = window.getSelection();
             //if (selection.rangeCount !== 0) {
             //    var range = selection.getRangeAt(0);
             //    var begin_word_el = that._selectParentNodeUntilWord(range.startContainer);
             //    var end_word_el = that._selectParentNodeUntilWord(range.endContainer);
-            //    
+            //
             //    var is_passage_selected = (
             //        !!range
             //        &&
@@ -157,7 +168,7 @@ var ReadAlong = {
             //        &&
             //        end_word_el
             //    );
-            //    
+            //
             //    if (is_passage_selected) {
             //        //that.current_begin_word = begin_word_node;
             //        //that.current_end_word = end_word_node;
@@ -167,11 +178,11 @@ var ReadAlong = {
             //    }
             //
             //}
-            
+
             that.selectCurrentWord();
             that.text_element.classList.add('speaking');
-        });
-        
+        }, false);
+
         /**
          * Abandon seeking the next word because we're paused
          */
@@ -179,7 +190,7 @@ var ReadAlong = {
             console.info(e.type); // debug
             that.selectCurrentWord(); // We always want a word to be selected
             that.text_element.classList.remove('speaking');
-            
+
             // if there was a selection, restore it.
             //if (that.current_selected_range !== null) {
             //    var selection = window.getSelection();
@@ -187,27 +198,46 @@ var ReadAlong = {
             //    selection.addRange(that.current_selected_range);
             //    that.current_selected_range = null;
             //}
-        });
-        
+        }, false);
+
         /**
-         * Seek by clicking (event delegation)
+         * Seek by selecting a word (event delegation)
          */
-        that.text_element.addEventListener('click', function(e) {
-            
+        function on_select_word_el(e) {
             if (!e.target.dataset.begin) {
                 return;
             }
             e.preventDefault();
-            
+
             //var selection = window.getSelection();
             //that.current_begin_word = e.target;
             //that.current_end_word = e.target;
-        
+
             var i = e.target.dataset.index;
             that.audio_element.currentTime = that.words[i].begin + 0.01; //Note: times apparently cannot be exactly set and sometimes select too early
             that.selectCurrentWord();
-        });
+        }
+        that.text_element.addEventListener('click', on_select_word_el, false);
+        that.text_element.addEventListener('keypress', function (e) {
+            if (e.keyCode === 13 /*Enter*/) {
+                on_select_word_el.call(this, e);
+            }
+        }, false);
         
+        /**
+         * Spacebar toggles playback
+         */
+        document.addEventListener('keypress', function (e) {
+            if (e.keyCode === 32 /*Space*/) {
+                if (that.audio_element.paused) {
+                    that.audio_element.play();
+                }
+                else {
+                    that.audio_element.pause();
+                }
+            }
+        });
+
         /**
          * First click handler sets currentTime to the word, and second click
          * here then causes it to play.
@@ -217,15 +247,15 @@ var ReadAlong = {
             console.info(e.type); // debug
             e.preventDefault();
             //that.audio_element.play();
-        });
-        
+        }, false);
+
         /**
          * Select a word when seeking
          */
         that.audio_element.addEventListener('seeked', function (e) {
             console.info(e.type); // debug
             that.selectCurrentWord();
-            
+
             /**
              * Address probem with Chrome where sometimes it seems to get stuck upon seeked:
              * http://code.google.com/p/chromium/issues/detail?id=99749
@@ -240,11 +270,17 @@ var ReadAlong = {
                     }
                 }, 500);
             }
-            
-            
-        });
+        }, false);
+
+        /**
+         * Select a word when seeking
+         */
+        that.audio_element.addEventListener('ratechange', function (e) {
+            console.info(e.type); // debug
+            that.selectCurrentWord();
+        }, false);
     },
-    
+
     _selectParentNodeUntilWord: function (node) {
         if ( !node || !this.text_element.contains(node) ){
             return null;
@@ -255,15 +291,4 @@ var ReadAlong = {
         }
         return this._selectParentNodeUntilWord(node.parentNode);
     }
-
 };
-
-
-
-// @todo Allow selection of a portion of text to have it read.  
-
-
-
-
-    
-
